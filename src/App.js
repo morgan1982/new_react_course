6356f582fc0ec76f2aea8da6213669a1f595296f
killerwhale <alexandrisanas@gmail.com>
2022-06-16 00:29:19 +0300

    adding props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Car Insuranse', amount: 294.4, date: new Date(2021, 2, 28)},
+    { title: 'SuperMarket', amount: 300, date: new Date(2021, 2, 29)},
+    { title: 'Taxes', amount: 500, date: new Date(2021, 3, 28)},
+    { title: 'Toys', amount: 25, date: new Date(2021, 3, 10)}
+  ]
 
   const numbers = [1, 2, 3, 4];
 
@@ -13,7 +19,9 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem/>
+      {expenses.map(({ title, amount, date }, i) => (
+        <ExpenseItem title={title} amount={amount} date={date} key={i}/>
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExprenseItem.css';
 
 const ExpenseItem = props => {
+  const {title, date, amount } = props;
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>{date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{amount}</div>
       </div>
     </div>
   )
