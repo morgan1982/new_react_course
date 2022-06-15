bd39e4b75928f3cbb0e52c66c65edff83ca5ba13
killerwhale <alexandrisanas@gmail.com>
2022-06-15 23:58:16 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+
+  const numbers = [1, 2, 3, 4];
+
+  const double = (nums) => nums.map(num => num * 2)
+
+  console.log(double(numbers))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem/>
     </div>
   );
 }
